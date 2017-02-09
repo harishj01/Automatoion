011196738054b13ebd3bbfbc299d9ba5dc7fcb39
harishj01 <harishj0119@gmail.com>
2017-02-09 15:38:37 +0530

    update

--- src/test/java/defaultpackages/NewTest.java
+++ src/test/java/defaultpackages/NewTest.java
@@ -1,27 +1,24 @@
-	package defaultpackages;	
+		package defaultpackages;	
 
 			
 	
 
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.firefox.FirefoxDriver;
-	import org.testng.Assert;		
+	
+	
+			
 	import org.testng.annotations.Test;	
 	import org.testng.annotations.BeforeTest;	
 	import org.testng.annotations.AfterTest;		
 	public class NewTest {		
-		    private WebDriver driver;		
+		   		
 			@Test				
 			public void testEasy() {	
-				driver.get("http://dixcyscott.biz");  
-				String title = driver.getTitle();				 
-				Assert.assertTrue(title.contains("DIXCY ::: Order Management System")); 
+				 
 				System.out.println("Sucess");
 			}	
 			@BeforeTest
 			public void beforeTest() {	
-				System.setProperty("webdriver.gecko.driver","C:\\Users\\Admin\\workspace\\geckodriver.exe");
-			    driver = new FirefoxDriver();  
+				  
 				System.out.println("  before test Sucess");
 
 			}		
@@ -29,6 +26,6 @@
 			public void afterTest() {
 				System.out.println("after test Sucess");
 
-				driver.quit();			
+							
 			}		
 	}	
